514cb9e583514d9d6ca285efc1422824486e7b2e
compCoder007 <competentcoder007@gmail.com>
2023-08-23 01:18:28 +0530

    replaced radios with labels

--- T2(ENGAGING_QUIZ)/index.js
+++ T2(ENGAGING_QUIZ)/index.js
@@ -4,7 +4,7 @@ test();
 
 
 function init(){
-    qnumber=[1,2,3,4];
+    
     quesions = ["one","two","three","four"];
     answers = ['C','D','A','B'];
     options = {0:[11,12,13,14],1:[21,22,23,24],2:[31,32,33,34],3:[41,42,43,44]};
@@ -13,28 +13,49 @@ function init(){
         usr_ans[k]=-1;
     }
     i=0;
+    
 
     show_options(0);    
 }
 
 function test(){
 
+    document.querySelectorAll("label").forEach((item) => {
+        item.addEventListener('click', function(){
+            item.style.backgroundColor="#999";
+            item.style.color="#fff";
+        })
+    })
+
+    document.getElementById("reset").addEventListener("click",function(){
+        radioLabels = document.querySelectorAll('.radio');
+        for(const rlabel of radioLabels){
+            rlabel.style.backgroundColor="#fff";
+            rlabel.style.color="#222";
+        }
+        
+    });
+
     document.getElementById("next").addEventListener("click",function(){
         save_answer(i);
         i=(i+1)%quesions.length;
-        show_options(i);
         document.getElementById("reset").click();
+        show_options(i);
         if(usr_ans[i]!=-1){
             document.getElementById(usr_ans[i].id).checked=true;
+            document.querySelector('label[for='+usr_ans[i].id+']').style.backgroundColor="#999";
+            document.querySelector('label[for='+usr_ans[i].id+']').style.color="#fff";
         }
     });
     document.getElementById("previous").addEventListener("click",function(){
         save_answer(i);
         i=i>0?i-1:quesions.length-1;
-        show_options(i);
         document.getElementById("reset").click();
+        show_options(i);
         if(usr_ans[i]!=-1){
             document.getElementById(usr_ans[i].id).checked=true;
+            document.querySelector('label[for='+usr_ans[i].id+']').style.backgroundColor="#999";
+            document.querySelector('label[for='+usr_ans[i].id+']').style.color="#fff";
         }
     });
     document.getElementById("submit").onclick = submit_quiz;
@@ -60,7 +81,7 @@ function save_answer(i){
 
 function show_options(i){
 
-    document.getElementById("qno").innerHTML=qnumber[i];
+
     document.getElementById("question").innerHTML=quesions[i];
 
     let j=0;
@@ -69,6 +90,7 @@ function show_options(i){
         rlabel.innerHTML = options[i][j];
         j++;
     }
+
 }
 
 function submit_quiz(){
@@ -86,6 +108,7 @@ function submit_quiz(){
     document.getElementById("previous").addEventListener("click",function(){
         show_answer(i);
     });
+    while(i!=0){document.getElementById("next").click()}
 
     
 
@@ -116,15 +139,26 @@ function show_answer(i){
         document.getElementById("feedback").innerHTML="Correct Answer! You got 1 point";
     }
     else{
-        document.getElementById("feedback").style.color="red";
-        document.getElementById("feedback").innerHTML="Wrong Answer! You got 0 points";
+        if(usr_ans[i]==-1){
+            document.getElementById("feedback").style.color="red";
+            document.getElementById("feedback").innerHTML="Unattempted! You got 0 points";
+        }
+        else{
+            document.getElementById("feedback").style.color="red";
+            document.getElementById("feedback").innerHTML="Wrong Answer! You got 0 points";
+        }
+        
     }
 }
 
 function report(){
-    document.getElementById("test_page").style.visibility="hidden";
+    
+    document.getElementById("test_page").style.display="none";
+    
+
 
     const htag = document.createElement("h2");
+    htag.style.color="#001e4d";
     htag.innerHTML="You've scored "+calculate_result()+" out of "+quesions.length+"!";
     
 
@@ -134,7 +168,10 @@ function report(){
     document.getElementById("report").append(htag,but);
 
     but.onclick=function(){
+        
+        document.getElementById("test_page").style.display="";
         document.getElementById("submit").innerHTML="View ScoreCard";
+        document.getElementById("submit").style.width="200px";
         document.getElementById("test_page").style.visibility="visible";
         document.getElementById("report").removeChild(htag);
         document.getElementById("report").removeChild(but);
@@ -142,6 +179,3 @@ function report(){
     };
 }
 
-function generate_report(){
-    document.getElementById("report").ap
-}
