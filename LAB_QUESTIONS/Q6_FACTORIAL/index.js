55140aae650406bbe92fae083426a991687a08c6
compCoder007 <competentcoder007@gmail.com>
2023-08-24 09:51:29 +0530

    q5 updated

---
+++ LAB_QUESTIONS/Q6_FACTORIAL/index.js
@@ -0,0 +1,12 @@
+number = document.getElementById("input");
+result = document.getElementById("result");
+
+document.getElementById("result").innerHTML=number.valueAsNumber;
+
+document.getElementById("factorial").addEventListener("click",factorial);
+
+
+
+function factorial(){
+    re
+}
